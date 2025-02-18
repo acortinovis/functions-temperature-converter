6b68cead0e19bd12a2daaea4ebcd344bb8df179a
acortinovis <adelio.cortinovis@gmail.com>
2025-02-18 10:54:13 -0800

    finished program
    finished program

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ def get_temp(temp: float, unit: str):
     # If unit is 'C', convert to Fahrenheit
         if unit.upper()=="C":
             converted=(temp *(9/5)) + 32
-            return (converted,"F")     # If unit is 'F', convert to Celsius
+            return converted,"F"    # If unit is 'F', convert to Celsius
         elif unit.upper()=="F":
             converted=(temp - 32) * (5/9)
-            return(converted "C")
+            return converted,"C"
         else:
             return None
 # Return a formatted string with the result
